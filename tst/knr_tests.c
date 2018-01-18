315a13313e152f88136cd82e2ff6e5609324518a
Philipp Fehre <philipp@fehre.co.uk>
2018-01-18 08:32:57 +0000

    testing setup for knr

--- tst/knr_tests.c
+++ tst/knr_tests.c
@@ -3,21 +3,14 @@
 #include "minunit.h"
 #include "knr.h"
 
-char* test_foo() {
-  int foo = 7;
-  mu_assert(foo == 7, "error, foo != 7");
-  return NULL;
-}
-
 char* test_f_to_c() {
-  float f = 32.0;
-
+  mu_assert(0.0 == knr_f_to_c(32.0), "error, foo != 7");
   return NULL;
 }
 
 char* all_tests() {
   mu_suite_start();
-  mu_run_test(test_foo);
+  mu_run_test(test_f_to_c);
   return NULL;
 }
 
